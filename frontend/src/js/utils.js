d63d11caac9ce5fb6a5b57d95a23f034739db3c1
shadow-lad <shardav.lad21@gmail.com>
2021-07-10 22:47:53 +0530

    ⚠ : Added a log to check why the error is occuring

--- frontend/src/js/utils.js
+++ frontend/src/js/utils.js
@@ -9,5 +9,12 @@ export const toastMessage = (message) => {
 };
 
 export const timeFormat = (time) => {
-	return new Date(time * 1000).toISOString().substr(11, 8);
+	try {
+		const formatted = new Date(time * 1000).toISOString().substr(11, 8);
+		return formatted;
+	} catch (e) {
+		console.log("Time:", time);
+		console.error(e);
+		return "";
+	}
 };
